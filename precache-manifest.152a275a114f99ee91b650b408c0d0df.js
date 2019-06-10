f48e28a4ebe440248605e7e09d74a698739637f2
kart <kartik.gupta@mavs.uta.edu>
2019-06-09 22:08:50 -0500

    React Capability Added

---
+++ precache-manifest.152a275a114f99ee91b650b408c0d0df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88d53b11fd3678114f13c1b67400611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c687328f6bdd53328a94",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c687328f6bdd53328a94",
+    "url": "/static/js/main.9a01d1a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
